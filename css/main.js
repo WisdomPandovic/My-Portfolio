bea2b69e014fd70a8ad6ce1965e03fc6e5497591
pandovic <wisdompandovic@gmail.com>
2023-11-01 09:15:39 +0100

    first commit

---
+++ css/main.js
@@ -0,0 +1,59 @@
+function showForm() {
+    document.getElementById('popup-form').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closeForm() {
+    document.getElementById('popup-form').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function showFormone() {
+    document.getElementById('popup-form-one').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closeFormone() {
+    document.getElementById('popup-form-one').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function showFormtwo() {
+    document.getElementById('popup-form-two').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closeFormtwo() {
+    document.getElementById('popup-form-two').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function showFormthree() {
+    document.getElementById('popup-form-three').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closeFormthree() {
+    document.getElementById('popup-form-three').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function showFormfour() {
+    document.getElementById('popup-form-four').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closeFormfour() {
+    document.getElementById('popup-form-four').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function showFormfive() {
+    document.getElementById('popup-form-five').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closeFormfive() {
+    document.getElementById('popup-form-five').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
